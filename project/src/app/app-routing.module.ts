9605f73194e1cbd5ea474704557989fdf1376db0
Ankita Sinha <sinhaankita2618@gmail.com>
2021-04-28 23:30:56 +0530

    create routes and component

--- project/src/app/app-routing.module.ts
+++ project/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AllUsersComponent } from './all-users/all-users.component';
+import { CreateUserComponent } from './create-user/create-user.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component'
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'', redirectTo:'create-user', pathMatch:'full'},
+  {path:'create-user', component:CreateUserComponent},
+  {path:'all-users', component:AllUsersComponent},
+  {path:'**', component:PageNotFoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CreateUserComponent } from './create-user/create-user.component';
+import { AllUsersComponent } from './all-users/all-users.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateUserComponent,
+    AllUsersComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
